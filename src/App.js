3732c8adb18c396767f14270ec2f8f1bdec14b16
Rick Lopez <ricklopez@users.noreply.github.com>
2021-01-08 18:56:53 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <AppProvider>
       <div className="App">
-        <QuickForm />
+        <QuickForm/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <FlavorList/>
